16a40c0cd314cb9cdf4bfe8dda99ad9e425651dc
jedihilario <hyanezbertola@gmail.com>
2026-09-14 22:19:20 -0300

    feat: Modularize styles and add ProA stuff

--- background.js
+++ background.js
@@ -7,6 +7,8 @@ window.addEventListener('DOMContentLoaded', () => {
         minHeight: 200.00,
         minWidth: 200.00,
         scale: 1.00,
-        scaleMobile: 1.00
+        scaleMobile: 1.00,
+        color: '#902923',
+        backgroundColor: '#212227'
     });
 });
